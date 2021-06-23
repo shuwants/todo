d1922c57c53f6325e2e1a4274973d64ec20cfeda
Shuji Kato <shuwants@gmail.com>
2021-06-24 01:10:29 +0900

    Updating checkbox connect to DB

--- app.py
+++ app.py
@@ -44,6 +44,21 @@ def create_todo():
         return jsonify(body)
 
 
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):  # Ajax requestから来る completed propertyの1つ
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect({url_for('index')})
+
+
 @app.route('/')
 def index():  # matching the name of route and as well as the name of route handler(@app.route('/')).
     return render_template('index.html', data=Todo.query.all())
