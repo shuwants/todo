bcc9d92bae4d1a40f8702a76030e6833a7b7908a
Shuji Kato <shuwants@gmail.com>
2021-07-11 17:41:51 +0900

    Closing modeling relationship -nullable False

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ class Todo(db.Model):  # このコードはtableを作らない。最初にtodoa
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
     list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'),
-                        nullable=True)
+                        nullable=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
